cded8777a3a638d52e818a6a87e7f19b829488bb
nonofare <adik96885@gmail.com>
2024-11-04 02:08:34 +0100

    Change method names

--- Binary_Search_Tree/BST.h
+++ Binary_Search_Tree/BST.h
@@ -11,13 +11,14 @@ namespace BST {
 
 			T data;
 			size_t index;
+
 			Node<T>* parent;
 			Node<T>* left;
 			Node<T>* right;
 
-			Node(T d, size_t i) {
-				data = d;
-				index = i;
+			Node(T inData, size_t inIndex) {
+				data = inData;
+				index = inIndex;
 				parent = nullptr;
 				left = nullptr;
 				right = nullptr;
@@ -25,38 +26,34 @@ namespace BST {
 		};
 
 		size_t size;
-		size_t max_index;
+		size_t next_index;
+
 		Node<T>* root;
 
-		size_t height(Node<T>* current) {
-			if (current == nullptr) {
+		int HeightOf(Node<T>* node) const {
+			if (node == nullptr) {
 				return 0;
 			}
 
-			size_t n1 = 0, n2 = 0;
-			if (current->right != nullptr) {
-				n1 = height(current->right);
+			int rightCount = 0, leftCount = 0;
+			if (node->right != nullptr) {
+				rightCount = HeightOf(node->right);
 			}
-			if (current->left != nullptr) {
-				n2 = height(current->left);
+			if (node->left != nullptr) {
+				leftCount = HeightOf(node->left);
 			}
 
-			if (n1 > n2) {
-				return 1 + n1;
-			}
-			else {
-				return 1 + n2;
-			}
+			return ((leftCount > rightCount) ? leftCount : rightCount) + 1;
 		}
 
-		bool dock(Node<T>* newNode, Node<T>* current, bool(*cmp)(T, T)) {
-			if (newNode == nullptr) {
+		bool FindSpot(Node<T>* seeker, Node<T>* current, bool (*cmp)(T, T)) {
+			if (seeker == nullptr) {
 				return false;
 			}
 
 			if (current == nullptr) {
 				if (root == nullptr) {
-					root = newNode;
+					root = seeker;
 					size++;
 					return true;
 				}
@@ -64,42 +61,42 @@ namespace BST {
 			}
 
 			if (cmp != nullptr) {
-				if (cmp(newNode->data, current->data)) {
-					if (current->right != nullptr) {
-						return dock(newNode, current->right, cmp);
+				if (cmp(seeker->data, current->data)) {
+					if (current->right == nullptr) {
+						current->right = seeker;
+						seeker->parent = current;
 					}
 					else {
-						current->right = newNode;
-						newNode->parent = current;
+						return FindSpot(seeker, current->right, cmp);
 					}
 				}
 				else {
-					if (current->left != nullptr) {
-						return dock(newNode, current->left, cmp);
+					if (current->left == nullptr) {
+						current->left = seeker;
+						seeker->parent = current;
 					}
 					else {
-						current->left = newNode;
-						newNode->parent = current;
+						return FindSpot(seeker, current->left, cmp);
 					}
 				}
 			}
 			else if constexpr (std::is_arithmetic_v<T>) {
-				if (newNode->data > current->data) {
-					if (current->right != nullptr) {
-						return dock(newNode, current->right, cmp);
+				if (seeker->data > current->data) {
+					if (current->right == nullptr) {
+						current->right = seeker;
+						seeker->parent = current;
 					}
 					else {
-						current->right = newNode;
-						newNode->parent = current;
+						return FindSpot(seeker, current->right, cmp);
 					}
 				}
 				else {
-					if (current->left != nullptr) {
-						return dock(newNode, current->left, cmp);
+					if (current->left == nullptr) {
+						current->left = seeker;
+						seeker->parent = current;
 					}
 					else {
-						current->left = newNode;
-						newNode->parent = current;
+						return FindSpot(seeker, current->left, cmp);
 					}
 				}
 			}
@@ -111,100 +108,99 @@ namespace BST {
 			return true;
 		}
 
-		bool flush(Node<T>* current) {
-			if (current == nullptr) {
-				return false;
-			}
-
-			if (current->left != nullptr) {
-				flush(current->left);
-			}
-
-			if (current->right != nullptr) {
-				flush(current->right);
-			}
-
-			if (current->parent != nullptr) {
-				if (current->parent->right == current) {
-					current->parent->right = nullptr;
-				}
-				else {
-					current->parent->left = nullptr;
-				}
-			}
-			else {
-				root = nullptr;
-			}
-
-			delete current;
-			size--;
-
-			return true;
-		}
-
-		bool remove(Node<T>* current) {
-			if (current == nullptr) {
+		bool Remove(Node<T>* node) {
+			if (node == nullptr) {
 				return false;
 			}
 
 			Node<T>* temp = nullptr;
-			if (current->left != nullptr) {
-				temp = max_right(current->left);
+			if (node->left != nullptr) {
+				temp = MaxRight(node->left);
 
 				if (temp != nullptr) {
-					current->left->parent = temp;
-					temp->left = current->left;
+					node->left->parent = temp;
+					temp->left = node->left;
 				}
 			}
-			else if (current->right != nullptr) {
-				temp = max_left(current->right);
+			else if (node->right != nullptr) {
+				temp = MaxLeft(node->right);
 
 				if (temp != nullptr) {
-					current->right->parent = temp;
-					temp->right = current->right;
+					node->right->parent = temp;
+					temp->right = node->right;
 				}
 			}
 
-			if (current->parent != nullptr) {
-				if (current->parent->right == current) {
-					current->parent->right = temp;
+			if (node->parent != nullptr) {
+				if (node->parent->right == node) {
+					node->parent->right = temp;
 				}
 				else {
-					current->parent->left = temp;
+					node->parent->left = temp;
 				}
 				if (temp != nullptr) {
-					temp->parent = current->parent;
+					temp->parent = node->parent;
 				}
 			}
 			else {
 				root = temp;
 			}
 
-			delete current;
+			delete node;
 			size--;
 
 			return true;
 		}
 
-		Node<T>* search(T data, Node<T>* current, bool(*cmp)(T, T)) {
-			if (current == nullptr || current->data == data) {
-				return current;
+		bool FlushUnder(Node<T>* node) {
+			if (node == nullptr) {
+				return false;
+			}
+
+			if (node->left != nullptr) {
+				FlushUnder(node->left);
+			}
+			if (node->right != nullptr) {
+				FlushUnder(node->right);
+			}
+
+			if (node->parent != nullptr) {
+				if (node->parent->right == node) {
+					node->parent->right = nullptr;
+				}
+				else {
+					node->parent->left = nullptr;
+				}
+			}
+			else {
+				root = nullptr;
+			}
+
+			delete node;
+			size--;
+
+			return true;
+		}
+
+		Node<T>* SearchFor(T data, Node<T>* node, bool (*cmp)(T, T)) const {
+			if (node->data == data || node == nullptr) {
+				return node;
 			}
 
 			if (cmp != nullptr) {
-				if (cmp(data, current->data)) {
-					return search(data, current->right, cmp);
+				if (cmp(data, node->data)) {
+					return SearchFor(data, node->right, cmp);
 				}
 				else {
-					return search(data, current->left, cmp);
+					return SearchFor(data, node->left, cmp);
 				}
 			}
 			else if constexpr (std::is_arithmetic_v<T>) {
-				if (data > current->data) {
-					return search(data, current->right, cmp);
+				if (data > node->data) {
+					return SearchFor(data, node->right, cmp);
 				}
 				else {
-					return search(data, current->left, cmp);
+					return SearchFor(data, node->left, cmp);
 				}
 			}
 			else {
@@ -212,76 +208,118 @@ namespace BST {
 			}
 		}
 
-		Node<T>* max_right(Node<T>* current) {
-			if (current == nullptr) {
+		Node<T>* MaxRight(Node<T>* node) const {
+			if (node == nullptr) {
 				return nullptr;
 			}
 
-			if (current->right == nullptr) {
-				if (current->left != nullptr) {
-					return max_right(current->left);
+			if (node->right == nullptr) {
+				if (node->left != nullptr) {
+					return MaxRight(node->left);
 				}
-				return current;
+				return node;
 			}
 			else {
-				return max_right(current->right);
+				return MaxRight(node->right);
 			}
 		}
 
-		Node<T>* max_left(Node<T>* current) {
-			if (current == nullptr) {
+		Node<T>* MaxLeft(Node<T>* node) const {
+			if (node == nullptr) {
 				return nullptr;
 			}
 
-			if (current->left == nullptr) {
-				if (current->right != nullptr) {
-					return max_left(current->right);
+			if (node->left == nullptr) {
+				if (node->right != nullptr) {
+					return MaxLeft(node->right);
 				}
-				return current;
+				return node;
 			}
 			else {
-				return max_left(current->left);
+				return MaxLeft(node->left);
 			}
 		}
 
-		std::string collect_strings(Node<T>* current, std::string(*out_string)(T), bool limited, size_t operation = 1) {
-			if (current == nullptr) {
+		std::string PreorderTraversal(Node<T>* node) const {
+			if (node == nullptr) {
 				return "";
 			}
 
 			std::string text = "[ Node: *";
-			text += std::to_string(int(current->index));
+			text += std::to_string(int(node->index));
+			text += " ]\n";
+
+			if (node->left != nullptr) {
+				text += PreorderTraversal(node->left);
+			}
+			if (node->right != nullptr) {
+				text += PreorderTraversal(node->right);
+			}
+
+			return text;
+		}
+
+		std::string InorderTraversal(Node<T>* node) const {
+			if (node == nullptr) {
+				return "";
+			}
+
+			std::string text = "";
+			if (node->left != nullptr) {
+				text += InorderTraversal(node->left);
+			}
+
+			text += "[ Node: *";
+			text += std::to_string(int(node->index));
+			text += " ]\n";
+
+			if (node->right != nullptr) {
+				text += InorderTraversal(node->right);
+			}
+
+			return text;
+		}
+
+		std::string CollectStrings(Node<T>* node, bool limited, std::string(*str)(T), int operation) const {
+			if (node == nullptr) {
+				return "";
+			}
+
+			const int OPERATION_LIMIT = 2;
+
+			std::string text = "[ Node: *";
+			text += std::to_string(int(node->index));
 
 			text += " | Data: ";
-			if (out_string != nullptr) {
-				text += out_string(current->data);
+			if (str != nullptr) {
+				text += str(node->data);
 			}
 			else if constexpr (std::is_arithmetic_v<T>) {
-				text += std::to_string(current->data);
+				text += std::to_string(node->data);
 			}
 			else {
 				text = "Data type is not supported and no method was provided";
 			}
 
 			text += " | Parent: *";
-			if (current->parent != nullptr) {
-				text += std::to_string(int(current->parent->index));
+			if (node->parent != nullptr) {
+				text += std::to_string(int(node->parent->index));
 			}
 			else {
 				text += "NL";
 			}
 
 			text += " | Left: *";
-			if (current->left != nullptr) {
-				text += std::to_string(int(current->left->index));
+			if (node->left != nullptr) {
+				text += std::to_string(int(node->left->index));
 			}
 			else {
 				text += "NL";
 			}
 
 			text += " | Right: *";
-			if (current->right != nullptr) {
-				text += std::to_string(int(current->right->index));
+			if (node->right != nullptr) {
+				text += std::to_string(int(node->right->index));
 			}
 			else {
 				text += "NL";
@@ -289,55 +327,15 @@ namespace BST {
 
 			text += " ]\n";
 
-			if (limited && operation > 2) {
+			if (limited && operation > OPERATION_LIMIT) {
 				return text;
 			}
 
-			if (current->left != nullptr) {
-				text += collect_strings(current->left, out_string, limited, operation++);
+			if (node->left != nullptr) {
+				text += CollectStrings(node->left, limited, str, operation++);
 			}
-			if (current->right != nullptr) {
-				text += collect_strings(current->right, out_string, limited, operation++);
-			}
-
-			return text;
-		}
-
-		std::string preorder_strings(Node<T>* current) {
-			if (current == nullptr) {
-				return "";
-			}
-
-			std::string text = "[ Node: *";
-			text += std::to_string(int(current->index));
-			text += " ]\n";
-
-			if (current->left != nullptr) {
-				text += preorder_strings(current->left);
-			}
-			if (current->right != nullptr) {
-				text += preorder_strings(current->right);
-			}
-
-			return text;
-		}
-
-		std::string inorder_strings(Node<T>* current) {
-			if (current == nullptr) {
-				return "";
-			}
-
-			std::string text = "";
-			if (current->left != nullptr) {
-				text += inorder_strings(current->left);
-			}
-
-			text += "[ Node: *";
-			text += std::to_string(int(current->index));
-			text += " ]\n";
-
-			if (current->right != nullptr) {
-				text += inorder_strings(current->right);
+			if (node->right != nullptr) {
+				text += CollectStrings(node->right, limited, str, operation++);
 			}
 
 			return text;
@@ -346,76 +344,77 @@ namespace BST {
 	public:
 		BinSerTree() {
 			size = 0;
-			max_index = 0;
+			next_index = 0;
 			root = nullptr;
 		}
 
 		~BinSerTree() {
-			erase();
+			FlushUnder(root);
 		}
 
-		bool push(T data, bool(*cmp)(T, T) = nullptr) {
-			Node<T>* newNode = new Node<T>(data, max_index++);
-			if (dock(newNode, root, cmp)) {
+		size_t Size() const {
+			return size;
+		}
+
+		size_t Height() const {
+			return HeightOf(root);
+		}
+
+		bool IsEmpty() const {
+			return size == 0;
+		}
+
+		bool Push(T data, bool (*cmp)(T, T) = nullptr) {
+			Node<T>* node = new Node<T>(data, next_index++);
+
+			if (FindSpot(node, root, cmp)) {
 				return true;
 			}
 			else {
-				delete newNode;
+				delete node;
 				return false;
 			}
 		}
 
-		Node<T>* find(T data, bool(*cmp)(T, T) = nullptr) {
-			return search(data, root, cmp);
+		bool Pop(Node<T>* node) {
+			return Remove(node);
 		}
 
-		bool pop(Node<T>* node) {
-			return remove(node);
+		bool Erase() {
+			return FlushUnder(root);
 		}
 
-		std::string preorder() {
+		Node<T>* Find(T data, bool (*cmp)(T, T) = nullptr) const {
+			return SearchFor(data, root, cmp);
+		}
+
+		std::string Preorder() const {
 			std::string text = "Preorder:\n";
 
 			text += "{\n";
-			text += preorder_strings(root);
+			text += PreorderTraversal(root);
 			text += "}\n";
 
 			return text;
 		}
 
-		std::string inorder() {
+		std::string Inorder() const {
 			std::string text = "Inorder:\n";
 
 			text += "{\n";
-			text += inorder_strings(root);
+			text += InorderTraversal(root);
 			text += "}\n";
 
 			return text;
 		}
 
-		bool erase() {
-			return flush(root);
-		}
-
-		bool is_empty() {
-			return size == 0;
-		}
-
-		size_t get_size() {
-			return size;
-		}
-
-		size_t get_height() {
-			return height(root);
-		}
-
-		std::string to_string(bool limited = false, std::string(*out_string)(T) = nullptr) {
+		std::string ToString(bool limited = false, std::string(*str)(T) = nullptr) const {
 			std::string text = "Binary Search Tree:\n";
 			text += "size: " + std::to_string(int(size)) + "\n";
-			text += "height: " + std::to_string(int(get_height())) + "\n";
+			text += "height: " + std::to_string(int(HeightOf(root))) + "\n";
 			text += "{\n";
 
-			text += collect_strings(root, out_string, limited);
+			text += CollectStrings(root, limited, str, 1);
 
 			if (limited) {
 				text += "[...]\n";
--- Binary_Search_Tree/Binary_Search_Tree.cpp
+++ Binary_Search_Tree/Binary_Search_Tree.cpp
@@ -5,33 +5,39 @@
 #include <time.h>
 #include "BST.h"
 
-// Does not wor: remove, to_string, main
+// Does not work: Remove, ToString, main
 
-int main2()
+int main()
 {
 	BST::BinSerTree<int>* bst = new BST::BinSerTree<int>();
 
-	bst->push(10);
-	bst->push(2);
-	bst->push(15);
-	bst->push(42);
-	bst->push(8);
-	bst->push(17);
-	bst->push(10);
-	bst->push(38);
-	bst->push(0);
-	bst->push(15);
+	bst->Push(10);
+	bst->Push(2);
+	bst->Push(15);
+	bst->Push(42);
+	bst->Push(8);
+	bst->Push(17);
+	bst->Push(10);
+	bst->Push(38);
+	bst->Push(0);
+	bst->Push(15);
 
-	std::cout << bst->to_string(true);
+	std::cout << bst->ToString(true);
 
-	//bst->pop(bst->find(2));
+	//bst->Pop(bst->find(2));
 
-	std::cout << bst->to_string(true);
+	std::cout << bst->ToString(true);
+
+	bst->Erase();
+
+	std::cout << bst->ToString(true);
 
 	delete bst;
 
 	return 0;
 }
+
+// --------------------------------------------------------------------------------------------- //
 
 struct some_object {
 	int field_1;
@@ -51,7 +57,7 @@ bool some_objects_cmp2(some_object* so1, some_object* so2) {
 	return so1->field_1 >= so2->field_1;
 }
 
-int main() {
+int main2() {
 	srand(time(NULL));
 	const int MAX_ORDER = 7;
 	BST::BinSerTree<some_object*>* bst = new BST::BinSerTree<some_object*>();
@@ -65,13 +71,13 @@ int main() {
 			some_object* so = new some_object();
 			so->field_1 = rand() % 100;
 			so->field_2 = 'A' + rand() % 26;
-			bst->push(so);
+			bst->Push(so);
 		}
 		clock_t t2 = clock();
 		std::cout << "Element pushing phase done" << std::endl;
 
 		double time = double(t2 - t1) / CLOCKS_PER_SEC;
-		std::cout << bst->to_string(true, some_objects_str);
+		std::cout << bst->ToString(true, some_objects_str);
 		std::cout << "Pushing time: " << time << "s" << std::endl << std::endl;
 
 		const int m = pow(10, 4);
@@ -84,7 +90,7 @@ int main() {
 			so->field_1 = rand() % 100;
 			so->field_2 = 'A' + rand() % 26;
 
-			if (bst->find(so)) {
+			if (bst->Find(so)) {
 				hints++;
 			}
 
@@ -97,7 +103,7 @@ int main() {
 		std::cout << "Finding time: " << time << "s" << std::endl;
 		std::cout << "Hints: " << hints << std::endl << std::endl;
 
-		bst->erase();
+		bst->Erase();
 	}
 
 	delete bst;
